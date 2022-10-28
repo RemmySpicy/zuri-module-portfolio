d391cb50c838a9f7780b178cb8cf55144901a040
O.Remilekun <remmylknolaotan@gmail.com>
2022-10-28 22:02:27 +0100

    Clean up comments from code

--- server.js
+++ server.js
@@ -5,17 +5,7 @@ const path = require('path');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    // res.writeHead(200, {'Content-Type': 'text/html'})
-    // fs.readFile('index.html', (error, data) => {
-    //     if (error) {
-    //         res.writeHead(404)
-    //         res.write('Error: File Not Found!')
-    //     } else {
-    //         res.write(data)
-    //     }
-    //     res.end()
-    // })
-
+    
     let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url )
     let contentType = getContentType(filePath) || 'text/html';
     let emptyPagePath = path.join(__dirname, '', '404.html')
@@ -31,29 +21,18 @@ const server = http.createServer((req, res) => {
                 res.end()
             }
         }
-
+        
         if (!err) {
             res.writeHead(200, {'Content-Type': contentType})
             res.write(data)
             res.end()
         }
     })
-
-    // if (req === '/') {
-    //     const aboutFilePath = path.join(__dirname, 'about', 'about.html')
-    //     fs.readFile(aboutFilePath, 'utf8', (error, data) => {
-    //         res.writeHead(200, {'Content-Type': 'text/html'})
-    //         res.write(data)
-    //         res.end()
-    //     })
-    // }
 });
-
-// const contactFilePath = path.join(__dirname, 'contact', 'contact.html')
 
 const getContentType = (filePath) => {
     const extName = path.extname(filePath);
-
+    
     if (extName === '.js') return 'text/javascript';
     if (extName === '.css') return 'text/css';
     if (extName === '.jpg') return 'img/jpg';
@@ -64,3 +43,36 @@ server.listen(port, (error) => {
     if (error) console.log("Something went wrong", error)
     else console.log('Server is listening on port', port)
 });
+
+
+
+
+
+
+
+
+// res.writeHead(200, {'Content-Type': 'text/html'})
+// fs.readFile('index.html', (error, data) => {
+//     if (error) {
+//         res.writeHead(404)
+//         res.write('Error: File Not Found!')
+//     } else {
+//         res.write(data)
+//     }
+//     res.end()
+// })
+
+
+
+
+
+// const contactFilePath = path.join(__dirname, 'contact', 'contact.html')
+
+    // if (req === '/') {
+    //     const aboutFilePath = path.join(__dirname, 'about', 'about.html')
+    //     fs.readFile(aboutFilePath, 'utf8', (error, data) => {
+    //         res.writeHead(200, {'Content-Type': 'text/html'})
+    //         res.write(data)
+    //         res.end()
+    //     })
+    // }
