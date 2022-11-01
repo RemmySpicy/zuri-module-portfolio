7aca8cee873bba4e58d9939a540f2bac40fddc8e
O.Remilekun <remmylknolaotan@gmail.com>
2022-11-01 23:48:45 +0100

    Change port to 4000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const port = 3000;
+const port = 4000;
 
 const server = http.createServer((req, res) => {
     
